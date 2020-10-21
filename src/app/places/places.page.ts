ae071ccd5cd13ac6b9656e1231c5434de0dc3de5
unknown <cristianarmestotejedo@gmail.com>
2020-10-21 13:00:11 +0200

    Problema al cargar imagen

--- src/app/places/places.page.ts
+++ src/app/places/places.page.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlacesPage implements OnInit {
 
+
+  private places = [
+    {
+      id: '1',
+      title: 'Eiffel Tower',
+      imageURL: 'https://megaconstrucciones.net/images/monumentos/foto/torre-eiffel.jpg',
+      comments: ['Awesome place', 'wonderful experience']
+    },
+    {
+      id: '2',
+      title: 'Statue Of Liberty',
+      imageURL: 'https://upload.wikimedia.org/wikipedia/commons/4/49/Estatua_de_la_Libertad_New_York.jpg',
+      comments: ['Awesome place', 'wonderful experience']
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
